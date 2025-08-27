b972789de9f72f9fe8db2e1c72a1090b3e1d148f
zakky <fakhrimuzakki06@gmail.com>
2025-08-27 16:38:30 +0700

    feat(hero): menambahkan header hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 export const dynamic = "force-static";
 
+import Hero from "@/components/layouts/Hero";
 import Navbar from "@/components/layouts/Navbar";
 
 export default function Home() {
   return (
     <div className="max-w-screen-lg mx-auto">
       <Navbar />
+      <Hero />
     </div>
   );
 }
---
+++ components/layouts/Hero.tsx
@@ -0,0 +1,34 @@
+import HeroImage from "@/assets/images/hero-image.png";
+import Image from "next/image";
+
+const Hero = () => {
+  return (
+    <header className="min-h-[416px] px-4 md:px-8 lg:px-24 py-14 lg:py-16 flex items-center gap-[72px] flex-col-reverse md:flex-row bg-neural-silver">
+      {/* content section */}
+      <div className="space-y-3 text-center md:text-left">
+        <h1 className="text-3xl lg:text-4xl font-bold leading-relaxed">
+          Lessons and insights <br />
+          <span className="text-brand-primary">from 8 years</span>
+        </h1>
+        <p className="">
+          Where to grow your business as a photographer: site or social media?
+        </p>
+        <button className="btn-primary">Regiter</button>
+      </div>
+
+      {/* Hero image */}
+      <div className="">
+        <Image
+          src={HeroImage}
+          alt="Hero Image"
+          className=""
+          width={272}
+          height={283}
+          priority
+        />
+      </div>
+    </header>
+  );
+};
+
+export default Hero;
